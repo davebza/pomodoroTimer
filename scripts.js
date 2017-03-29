ba29f33171e7910b298191746e9a859d092f1143
Dave Brown <daveb.za@gmail.com>
2017-03-29 13:56:36 +0800

    Feat: added button to start countdown

--- scripts.js
+++ scripts.js
@@ -1,42 +1,56 @@
 // This file is for the JS that will be needed to run a pomodoro timer.
-
-//First, get the current time:
-var timerStartValue = Date.now();
-console.log(timerStartValue);
-var timerEndValue = timerStartValue + 1500000;
-var outputWithMoment = moment(timerEndValue).format("H:mm:ss:SSS");
-console.log(timerEndValue, outputWithMoment);
+timerStartValue = Date.now();
+timerEndValue = timerStartValue + 1500000;
+var outputWithMoment = moment(timerEndValue).format("H:mm:ss A");
+$('#endTimerActualTime').html("Your timer will end at "+outputWithMoment);
+var countdown = false;
 
 function timer(){
     var timerCountdown = timerEndValue - Date.now();
-    var timeOutput = msToTime(timerCountdown);
-    $('#timerOutput').html(timeOutput);
+    var timeOut = msToTime(timerCountdown);
+    $('#timerOutput').html(timeOut);
 }
 
-//Convert millisecs to minutes and secs (from Stackoverflow):
+//Convert millisecs to minutes and secs:
+//(from Stackoverflow:
+// http://stackoverflow.com/questions/19700283/how-to-convert-time-milliseconds-to-hours-min-sec-format-in-javascript):
 function msToTime(duration) {
-        var   seconds = parseInt((duration/1000)%60)
-            , minutes = parseInt((duration/(1000*60))%60)
-
-        minutes = (minutes < 10) ? "0" + minutes : minutes;
-        seconds = (seconds < 10) ? "0" + seconds : seconds;
-        return minutes + ":" + seconds
-    }
+    var   seconds = parseInt((duration/1000)%60);
+    var minutes = parseInt((duration/(1000*60))%60);
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    return minutes + ":" + seconds;
+}
 
 //render function:
 function render(){
-    now = moment().format("dddd, MMMM Do YYYY, h:mm:ss A");
+    now = moment().format("dddd, MMMM Do YYYY, H:mm:ss A");
     //Output to browser:
-    $('#realTimeOut').html(now);
+    $('#realTimeOut').html("The time is "+ moment().format("H:mm:ss A") + " on " + moment().format("dddd, MMMM Do YYYY"));
 }
 
+function update(){
+    if(countdown === true){
+        timer();
+    }
+}
+
+$('#startTimer').click(function(){
+    console.log("click");
+    timerStartValue = Date.now();
+    timerEndValue = timerStartValue + 1500000;
+    var outputWithMoment = moment(timerEndValue).format("H:mm:ss A");
+    $('#endTimerActualTime').html("Your timer will end at "+outputWithMoment);
+    countdown = true;
+    console.log(countdown);
+    return countdown;
+});
 
 //Main program calls:
-
-// First, call render function to set up the clock on page load
+// First, call render function to set up the clock on page load:
 render();
 // then update:
 //Actual Time:
 setInterval(render, 10);
 //The timer:
-setInterval(timer, 10);
+setInterval(update, 10);
