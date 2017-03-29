c1c21f7213bc99a47183ae358b76e393f57f4dc8
Dave Brown <daveb.za@gmail.com>
2017-03-29 12:25:21 +0800

    Feat: full real time clock and 25 min timer

--- scripts.js
+++ scripts.js
@@ -1,21 +1,42 @@
-// This file is for the JS that will be needed to run a pomodor timer.
+// This file is for the JS that will be needed to run a pomodoro timer.
 
 //First, get the current time:
+var timerStartValue = Date.now();
+console.log(timerStartValue);
+var timerEndValue = timerStartValue + 1500000;
+var outputWithMoment = moment(timerEndValue).format("H:mm:ss:SSS");
+console.log(timerEndValue, outputWithMoment);
 
-var now = moment();
-var time = now._d;
+function timer(){
+    var timerCountdown = timerEndValue - Date.now();
+    var timeOutput = msToTime(timerCountdown);
+    $('#timerOutput').html(timeOutput);
+}
+
+//Convert millisecs to minutes and secs (from Stackoverflow):
+function msToTime(duration) {
+        var   seconds = parseInt((duration/1000)%60)
+            , minutes = parseInt((duration/(1000*60))%60)
+
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+        return minutes + ":" + seconds
+    }
 
 //render function:
 function render(){
-    now = moment();
-    time = now._d;
+    now = moment().format("dddd, MMMM Do YYYY, h:mm:ss A");
     //Output to browser:
-    $('#realTimeOut').html(time);
-    console.log("rendered!");
+    $('#realTimeOut').html(now);
 }
 
-render();
-setInterval(render, 1000);
-//delta function:
 
-//Then, work elapsed time?
+//Main program calls:
+
+// First, call render function to set up the clock on page load
+render();
+// then update:
+//Actual Time:
+setInterval(render, 10);
+//The timer:
+setInterval(timer, 10);
