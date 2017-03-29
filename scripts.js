5172e34aae7cbea2b3f0e43a9657c3f2bd148f16
Dave Brown <daveb.za@gmail.com>
2017-03-29 08:57:10 +0800

    feat: Added a working 1 second update clock

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 // This file is for the JS that will be needed to run a pomodor timer.
+
+//First, get the current time:
+
+var now = moment();
+var time = now._d;
+
+//render function:
+function render(){
+    now = moment();
+    time = now._d;
+    //Output to browser:
+    $('#realTimeOut').html(time);
+    console.log("rendered!");
+}
+
+render();
+setInterval(render, 1000);
+//delta function:
+
+//Then, work elapsed time?
